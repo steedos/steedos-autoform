8d05a836364d6677a8d67524925f851190efbbef
baozhoutao <baozhoutao@hotoa.com>
2018-02-09 18:04:12 +0800

    update version

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
  */
 Package.describe({
 	name: 'steedos:autoform',
-	version: '0.0.9',
+	version: '0.0.10',
 	summary: 'Steedos libraries',
 	git: 'https://github.com/steedos/apps/tree/master/packages/steedos-autoform',
 	documentation: null
